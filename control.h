c3e969ddec56edd03d37306f56811f9b04a9fa90
MorphSight <irishsnowday@icloud.com>
2021-07-08 22:56:59 -0700

    Completed step 3
    Enum defined for control.
    
    Security Condition added.
    
    Authenticate Method now returns a control (will throw an exception if run, we need to now return the correct Control for the user).
    
    Control now has a member variable Control. We should change the constructor to also take in a control.

--- control.h
+++ control.h
@@ -8,5 +8,26 @@
  ************************************************************************/
 
 #pragma once
+/*
+ * Elements:
+ *      Control -            Permissions
+ *      Asset -              Message: Messages now have a member called control.
+ *      Subject Control -    User: Authenticate has been changed to return a Control
+ *      Security Condition - Defined below for reading and writing
+ *      Integration -        Handled in Step 4. Leverage the Security Condition.
+ */
 
-// you may need to put something here...
+enum Control {
+    PUBLIC = 0,
+    CONFIDENTIAL = 1,
+    PRIVILEDGED = 2,
+    SECRET = 3
+};
+
+bool securityConditionRead(Control asset, Control subject) {
+    return asset >= subject;
+}
+
+bool securityConditionWrite(Control asset, Control subject) {
+    return asset <= subject;
+}
